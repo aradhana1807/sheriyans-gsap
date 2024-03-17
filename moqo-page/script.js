e3976c307bb06170ec10e15c742ce305d289e961
aradhana1807 <aradhana.zero@gmail.com>
2024-03-17 15:10:07 +0530

    added moqo landing page + locomotive starter files

--- moqo-page/script.js
+++ moqo-page/script.js
@@ -1,3 +1,32 @@
+var tl = gsap.timeline()
+
+function createTimeLoader() {
+    let randomValue = 0
+    setInterval(function () {
+        randomValue = randomValue + Math.floor(Math.random() * 20);
+        if (randomValue < 100) {
+            document.querySelector('#loader h1').innerText = randomValue + '%';
+        }
+        else {
+            randomValue = 100;
+            document.querySelector('#loader h1').innerText = randomValue + '%';
+        }
+    }, 150);
+}
+
+
+tl.to("#loader h1", {
+    scale: 1.5,
+    delay: 0.5,
+    duration: 1,
+    onStart: createTimeLoader()
+})
+
+tl.to("#loader", {
+    top: "-100vh",
+    delay: 0.3,
+    duration: 1.5
+})
 
 
 gsap.to("#page1 h1", {
